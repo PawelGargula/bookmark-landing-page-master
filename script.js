7dbd19f3aba3c80fefeeda1a0b25230a5a938538
Paweł Gargula <pawel.gargula@interia.pl>
2022-07-26 22:15:53 +0200

    Add tabs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hamburger menu
 const menuButton = document.querySelector('.toggle-menu-button');
 const primaryHeader = document.querySelector('.primary-header');
 
@@ -9,4 +10,21 @@ menuButton.addEventListener('click', () => {
         menuButton.firstChild.src = 'images/icon-close.svg';
         primaryHeader.setAttribute('data-menu-open', '');
     }
+});
+
+// Tabs
+const tabHeaders = document.querySelectorAll('[data-header-nr]');
+const tabContents = document.querySelectorAll('[data-content-nr]');
+
+tabHeaders.forEach(tabHeader => {
+    tabHeader.addEventListener('click', () => {
+        // Remove active states on all tabs
+        tabContents.forEach(tabContent => tabContent.classList.remove('tab-content-active'));
+        tabHeaders.forEach(tabH => tabH.classList.remove('tab-header-active'));
+        
+        tabHeader.classList.add('tab-header-active');
+        
+        const tabContent = [...tabContents].find(tabC => tabC.dataset.contentNr === tabHeader.dataset.headerNr);
+        tabContent.classList.add('tab-content-active');
+    });
 });
